f1186f2014741467c8393964c422def2465f7b9b
François Cerbelle <francois@cerbelle.net>
2018-06-25 15:22:09 +0200

    Updated GitHub Certificate fingerprint

--- examples/Registration/GitHub.ino
+++ examples/Registration/GitHub.ino
@@ -38,7 +38,7 @@ MENUFCT(githubtest) {
   Serial.print("]... ");
   url = (String)"https://github.com/"
         + getCommandByName("githubuser")->value;
-  http.begin(url, "D7 9F 07 61 10 B3 92 93 E3 49 AC 89 84 5B 03 80 C1 9E 2F 8B");
+  http.begin(url, "CA 06 F5 6B 25 8B 7A 0D 4F 2B 05 47 09 39 47 86 51 15 19 84");
   httpCode = http.GET();
   http.end();
   if (httpCode != HTTP_CODE_OK) {
